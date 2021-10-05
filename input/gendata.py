8db7a9122f9496bba285fedb701d18b412712919
Jody Klymak <jklymak@gmail.com>
2021-10-05 04:27:23 -0700

    gendata for OneHill100lowU10N10Amp305f141B059Sm200: One 100 km radius hill, smoothed with var drag, fixed typo

--- input/gendata.py
+++ input/gendata.py
@@ -273,7 +273,7 @@ if patch:
 print(R)
 fig, ax = plt.subplots(2,1, constrained_layout=True)
 pcm=ax[1].pcolormesh(x/1.e3,y/1.e3,env,rasterized=True, 
-                    shading='auto', vmin=-4000, vmax=-3000)
+                    shading='auto', vmin=0, vmax=1)
 fig.savefig(outdir+'/figs/env.png')
 
 
