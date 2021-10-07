d849dfd8d80e8d7177d41aee712adbb84b644184
Jody Klymak <jklymak@gmail.com>
2021-10-07 01:12:45 -0700

    gendata for OneHill100lowU10N10Amp305f141B059Rough: One 100 km radius hill, smoothed with var drag, fixed typo

--- input/runAll.sh
+++ input/runAll.sh
@@ -2,9 +2,9 @@
 for todo in OneHill100lowU10N10Amp305f141B059Rough
 do
     day=86400
-    jobid=$(sbatch -J $todo --export=start=0,stop=$((day*6 + 180)),dt=180 runModel.sh)
+    jobid=$(sbatch -J $todo --export=start=0,stop=$((day*6 + 180)),dt=90 runModel.sh)
     #echo $jobid
-    jobid1=$(sbatch  -J $todo --dependency=afterok:${jobid##* } --export=start=$((day*6)),stop=$((day*12 + 180)),dt=180 runModel.sh)
+    jobid1=$(sbatch  -J $todo --dependency=afterok:${jobid##* } --export=start=$((day*6)),stop=$((day*12 + 180)),dt=90 runModel.sh)
     jobid2=$(sbatch  -J $todo --dependency=afterok:${jobid1##* } --export=start=$((day*12)),stop=$((day*18 + 180)),dt=90 runModel.sh)
     jobid3=$(sbatch  -J $todo --dependency=afterok:${jobid2##* } --export=start=$((day*18)),stop=$((day*24 + 180)),dt=90 runModel.sh)
     #jobid4=$(sbatch  -J $todo --dependency=afterok:${jobid3##* } --export=start=$((day*24)),stop=$((day*30 + 180)),dt=180 runModelRestarts.sh)
